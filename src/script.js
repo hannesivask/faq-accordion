6f733ad4e3168dc5967e1db5e7bdbc88f7cc7f2b
hannesivask <hannesegmond@gmail.com>
2023-12-20 19:10:16 +0200

    Update

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ const contents = document.querySelectorAll(".acc--content");
 contents.forEach(function (content) {
   const title = content.parentElement.children[0];
   title.addEventListener("click", function () {
+    this.focus;
     content.classList.toggle("hidden");
     content.classList.contains("hidden")
       ? (this.children[1].src = `assets/images/icon-plus.svg`)
